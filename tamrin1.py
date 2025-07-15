30069e3b1318cd7e1368c92d76340e402cdf17e0
m-zamani76 <220458868+m-zamani76@users.noreply.github.com>
2025-07-15 22:44:10 +0330

    Update tamrin1.py

--- tamrin1.py
+++ tamrin1.py
@@ -1,10 +1,28 @@
 import random
-
-name = input("Enter the name :").lower()
-age = input("Enter the age :")
-color = input("Enter the Favorite color :").upper()
-symbol= "@#$%&*"
-s= list(name)+ list(age)+ list(color)+ list(symbol)
-random.shuffle(s)
-r = "".join(s[ :12])
-print("Secure Password:" ,(r))
+while True:
+    try:
+        while True:
+            name = input("Enter the name :").lower().strip()
+            if name :
+                break
+            else:
+                print("Please Enter the name")
+        while True:
+            age = input("Enter the age :").strip()
+            if age:
+                break
+            else:
+                print("Please Enter the age")
+        while True:
+            color = input("Enter the Favorite color :").upper().strip()
+            if color:
+                break
+            else:
+                print("Please Enter the color")
+        symbol= "@#$%&*"
+        s= list(name)+ list(age)+ list(color)+ list(symbol)
+        random.shuffle(s)
+        r = "".join(s[ :12])
+        print("Secure Password:" ,(r))
+    except ValueError:
+        print("Please Enter the character")
